b7c66fe35708985d16d3b48afed041dd34725724
Victor Juarez <juarezv1990@gmail.com>
2022-11-14 18:36:43 -0500

    added more movie rows

--- src/Component/Request.js
+++ src/Component/Request.js
@@ -1,10 +1,12 @@
 const key = "bc09d8f6d7221844a572405e063008e4";
-const base = "https://api.themoviedb.org/3/movie/";
+const base = "https://api.themoviedb.org/3/movie";
 
 
 const request = {
     requestPopular: `${base}/popular?api_key=${key}&language=en-US&page=1`,
-    requestTrending: `${base}/popular?api_key=${key}&language=en-US&page=2`
+    requestTrending: `${base}/popular?api_key=${key}&language=en-US&page=2`,
+    requestTopRated: `${base}/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestNowPlaying: `${base}/now_playing?api_key=${key}&language=en-US&page=1`
 }
 
 export default request;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,8 @@ const Home = () => {
       <Main />
       <Row rowID='1' title='Popular' fetchURL={request.requestPopular} />
       <Row rowID='2' title='Trending' fetchURL={request.requestTrending} />
+      <Row rowID='3' title='Top Rated' fetchURL={request.requestTopRated} />
+      <Row rowID='4' title='Now Playing' fetchURL={request.requestNowPlaying} />
     </div>
   )
 }
