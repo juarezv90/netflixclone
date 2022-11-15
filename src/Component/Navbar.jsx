a6f45ffae557fb11391fe2f71d5f5babe6935483
Victor Juarez <juarezv1990@gmail.com>
2022-11-15 00:31:56 -0500

    login, signup , account page created, firebase sta

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between p-4 w-full z-[100] absolute'>
-      <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+    <div className="flex items-center justify-between p-4 w-full z-[100] absolute">
+      <Link to="/">
+        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
       <div>
-        <button className='text-white pr-4'>Sign In</button>
-        <button className='text-white bg-red-600 px-6 py-2 rounded cursor-pointer'>Sign Up</button>
+        <Link to="/login">
+          <button className="text-white pr-4">Sign In</button>
+        </Link>
+        <Link to="/signup">
+          <button className="text-white bg-red-600 px-6 py-2 rounded cursor-pointer">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <>
+    <div className="w-full h-screen ">
+      <img
+        className="hidden sm:block absolute w-full h-full object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/0ef67cc5-0aa7-47cf-87bd-7f595afc7cfb/728618d9-9274-46ab-9850-1015ff9c05df/US-en-20221107-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+        alt=""
+      />
+
+      <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+      <div className="fixed w-full px-4 py-24 z-50">
+        <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+          <div className="max-w-[320px] mx-auto py-16">
+            <h1 className="text-3xl font-bold">Sign In</h1>
+            <form className="w-full flex flex-col py-4">
+              <input
+                className="p-3 my-2 bg-gray-700 rounded"
+                type="email"
+                placeholder="Email"
+                autoComplete="email"
+              />
+              <input
+                className="p-3 my-2 bg-gray-700 rounded"
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+              />
+              <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                Sign In
+              </button>
+              <div className="flex justify-between items-center text-sm text-gray-600">
+                <p>
+                  <input type="checkbox" className="mr-2" />
+                  Remember Me
+                </p>
+                <p>Need Help?</p>
+              </div>
+              <p className="py-8">
+                <span className="text-gray-600">
+                  Already Subscribed to Netflix?
+                </span>{' '}
+                <Link to="/signup">Sign Up</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+  )
+}
+
+export default Login
